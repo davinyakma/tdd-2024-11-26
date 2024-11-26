0a0f9226da7a0feb18834619b2ea9633a2cb7735
davinyakma <poohcdv3652@naver.com>
2024-11-26 14:14:49 +0900

    feat:11-26_Calc_t1 red

---
+++ src/main/java/com/programmers/Calc.java
@@ -0,0 +1,9 @@
+package com.programmers;
+
+public class Calc {
+    //int rs = Calc.run("((3 + 5) * 5 + -10) * 10 / 5"); 구현
+    //분할정복이 가능하다 ex. 3 + 5 먼저 하고 *5 하는 식으로 어려운 문제를 쪼개기
+    public static int run(String expr){
+        return 8;
+    }
+}
---
+++ src/test/java/com/programmers/CalcTest.java
@@ -0,0 +1,16 @@
+package com.programmers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalcTest {
+    @Test
+    @DisplayName("10 + 5")
+    public void t1() {
+        int rs = Calc.run("10 + 5");
+
+        assertThat(rs).isEqualTo(15);
+    }
+}
