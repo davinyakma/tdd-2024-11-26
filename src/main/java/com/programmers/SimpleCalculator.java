cd8508fc724c706d1d9a2f9cabaafed986f9a557
davinyakma <poohcdv3652@naver.com>
2024-11-26 11:48:48 +0900

    feat:11-26_T1_Green_red completed and blue doesn't need

---
+++ src/main/java/com/programmers/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package com.programmers;
+
+public class SimpleCalculator {
+
+    public int plus(int num1, int num2) {
+        return 3; //일단 어떻게 테스트를 구현할지 몰라서 T1의 결과값 3이라고 두자.
+        //Green부분 완수 : 테스트를 꼼수를 부려서 통과하게만 하면 됨.
+        //SimpleCalculator simpleCalculator = new SimpleCalculator(); SimpleCalculator클래스가 없었으니까
+        //클래스와 plus메소드를 main폴더에 생성하고 구현은 하지말고 테스트를 통과할 값 3을 return함.
+        //Red부분 완수 : SimpleCalculatorTest클래스를 test폴더 안에 생성하여 1 + 2 = 3 인 테스트를 생성함.
+        //Blue부분 완수 : 리팩토링할 부분이 없이 코드가 짧아서 생략!
+    }
+}
