9594b710bab9b1136597ddc164e6edbf90a71712
davinyakma <poohcdv3652@naver.com>
2024-11-26 12:38:39 +0900

    feat:11-26_use assertThat().isEqualTo()

--- src/test/java/com/programmers/SimpleCalculatorTest.java
+++ src/test/java/com/programmers/SimpleCalculatorTest.java
@@ -3,7 +3,7 @@ package com.programmers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SimpleCalculatorTest {
     @Test //아래의 테스트가 독립적으로 실행이 가능하다.
@@ -12,7 +12,8 @@ public class SimpleCalculatorTest {
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         int rs = simpleCalculator.plus(1, 2);
 
-        assertEquals(3, rs); //expected, actual을 매개변수로 받는다.
+        //assertEquals(3, rs); //같은 것임. 아래 코드가 더 보기편한것 같음.
+        assertThat(rs).isEqualTo(3); //rs가 3과 똑같을 것을 단정적으로 말한다.
     }
     @Test //아래의 테스트가 독립적으로 실행이 가능하다.
     @DisplayName("2 + 10 = 12") //설명
@@ -20,7 +21,7 @@ public class SimpleCalculatorTest {
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         int rs = simpleCalculator.plus(2, 10);
 
-        assertEquals(12, rs); //expected, actual을 매개변수로 받는다.
+        assertThat(rs).isEqualTo(12);
     }
     @Test //아래의 테스트가 독립적으로 실행이 가능하다.
     @DisplayName("20 + 10 = 30") //설명
@@ -28,6 +29,6 @@ public class SimpleCalculatorTest {
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         int rs = simpleCalculator.plus(20, 10);
 
-        assertEquals(30, rs); //expected, actual을 매개변수로 받는다.
+        assertThat(rs).isEqualTo(30);
     }
 }
